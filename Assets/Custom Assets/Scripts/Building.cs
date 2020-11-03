7d4ea60c4ff02e031e00934cb4dc69edfb5d1e43
Erik Schreurs <erikschreurs91@gmail.com>
2020-11-03 23:20:59 +0100

    Basic grid and building placement

---
+++ Assets/Custom Assets/Scripts/Building.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public bool isSelected = false;
+    public bool canBuild = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider col) {
+        if (col.gameObject.name != "Grid") {
+            this.GetComponent<Renderer>().material.color = Color.red;
+            this.canBuild = false;
+        }
+    }
+    void OnTriggerExit(Collider col) {
+        if (isSelected) {
+            this.GetComponent<Renderer>().material.color = Color.green;
+            this.canBuild = true;
+        } else {
+            this.GetComponent<Renderer>().material.color = Color.white;
+            this.canBuild = true;
+        }
+    }
+}
